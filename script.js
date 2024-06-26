7fac62a0cd7233b5b52f8d983cbe6a812bde29ed
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-25 20:34:33 -0700

    Working operator buttons

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let calculator = document.querySelector("#calculator")
+let calculator = document.querySelector("#calculator");
+let string1 = "";
+let string2 = "";
+let operators = [];
+let display = document.querySelector("#displayText");
+
 
 function operate(val1, operator, val2) {
   if (operator === "+") {
@@ -10,9 +15,10 @@ function operate(val1, operator, val2) {
   } else if (operator === "/"){
     return val1 / val2;
   } else if (operator === "%"){
-    return val1 % val2;
+    return (val1 % val2);
   } else {
-    return "error";
+    alert("error");
+    return;
   }
 }
 
@@ -20,52 +26,208 @@ calculator.addEventListener("click", (event) =>{
   let target = event.target;
   switch(target.id) {
     case "one" :
-      console.log(1);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "1";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "1";
+        display.textContent = string2;
+      } else {
+        string1 += "1";
+        display.textContent = string1;
+      }
       break;
     case "two" :
-      console.log(2);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "2";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "2";
+        display.textContent = string2;
+      } else {
+        string1 += "2";
+        display.textContent = string1;
+      }
       break;
     case "three" :
-      console.log(3);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "3";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "3";
+        display.textContent = string2;
+      } else {
+        string1 += "3";
+        display.textContent = string1;
+      }
       break;
     case "four" :
-      console.log(4);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "4";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "4";
+        display.textContent = string2;
+      } else {
+        string1 += "4";
+        display.textContent = string1;
+      }
       break;
     case "five" :
-      console.log(5);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "5";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "5";
+        display.textContent = string2;
+      } else {
+        string1 += "5";
+        display.textContent = string1;
+      }
       break;
     case "six" :
-      console.log(6);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "6";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "6";
+        display.textContent = string2;
+      } else {
+        string1 += "6";
+        display.textContent = string1;
+      }
       break;
     case "seven" :
-      console.log(7);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "7";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "7";
+        display.textContent = string2;
+      } else {
+        string1 += "7";
+        display.textContent = string1;
+      }
       break;
     case "eight" :
-      console.log(8);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "8";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "8";
+        display.textContent = string2;
+      } else {
+        string1 += "8";
+        display.textContent = string1;
+      }
       break;
     case "nine" :
-      console.log(9);
+      if (operators[0] === "=") {
+        string1="";
+        operators.pop();
+        string1 += "9";
+        display.textContent = string1;
+      } else if (operators[0]) {
+        string2 += "9";
+        display.textContent = string2;
+      } else {
+        string1 += "9";
+        display.textContent = string1;
+      }
       break;
     case "zero" :
-      console.log(0);
+      if (operators[0]) {
+        string2 += "0";
+        display.textContent = string2;
+      } else {
+        string1 += "0";
+        display.textContent = string1;
+      }
       break;
     case "divide" :
-      console.log("/");
+      if (operators[0] && string2.length > 0) {
+        let value = operate(parseInt(string1),operators[0],parseInt(string2));
+        console.log(value);
+        string1 = "" + value;
+        string2 = "";
+        operators.pop();
+        operators[0] = "/";
+        display.textContent = string1;
+      } else {
+        operators[0] = "/";
+      }
       break;
     case "multiply" :
-      console.log("*");
+      if (operators[0] && string2.length > 0) {
+        let value = operate(parseInt(string1),operators[0],parseInt(string2));
+        console.log(value);
+        string1 = "" + value;
+        string2 = "";
+        operators.pop();
+        operators[0] = "*";
+        display.textContent = string1;
+      } else {
+        operators[0] = "*";
+      }
       break;
     case "add" :
-      console.log("+");
+      if (operators[0] && string2.length > 0) {
+        let value = operate(parseInt(string1),operators[0],parseInt(string2));
+        console.log(value);
+        string1 = "" + value;
+        string2 = "";
+        operators.pop();
+        operators[0] = "+";
+        display.textContent = string1;
+      } else {
+        operators[0] = "+";
+      }
       break;
     case "subtract" :
-      console.log("-");
+      if (operators[0] && string2.length > 0) {
+        let value = operate(parseInt(string1),operators[0],parseInt(string2));
+        console.log(value);
+        string1 = "" + value;
+        string2 = "";
+        operators.pop();
+        operators[0] = "-";
+        display.textContent = string1;
+      } else {
+        operators[0] = "-";
+      }
       break;
     case "equals" :
-      console.log("=");
+      if (operators[0] && string2.length > 0) {
+        let value = operate(parseInt(string1),operators[0],parseInt(string2));
+        console.log(value);
+        string1 = "" + value;
+        string2 = "";
+        operators.pop();
+        display.textContent = string1;
+        operators[0] = "=";
+      }
       break;
     case "ac" :
-      console.log("AC");
+      string1 = "";
+      string2 = "";
+      operators = [];
+      display.textContent = "";
       break;
     case "backspace" :
       console.log("C");
@@ -74,7 +236,17 @@ calculator.addEventListener("click", (event) =>{
       console.log(".")
       break;
     case "remainder" :
-      console.log("%");
+      if (operators[0] && string2.length > 0) {
+        let value = operate(parseInt(string1),operators[0],parseInt(string2));
+        console.log(value);
+        string1 = "" + value;
+        string2 = "";
+        operators.pop();
+        operators[0] = "%";
+        display.textContent = string1;
+      } else {
+        operators[0] = "%";
+      }
       break;
   }
 })
